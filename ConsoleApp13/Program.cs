b5374f9f6e1683a495032a4cf6bc5b8abb43bdfe
Samsung <Samsung@DESKTOP-IEPFAHM>
2024-10-20 18:08:10 +0200

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite vase ime");
+            string Ime = Console.ReadLine();
+            Console.WriteLine("Koliko imate godina");
+            int godine = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kojeg ste pola M/Z");
+            string pol = Console.ReadLine();
+
+
+
+
+
+            if (pol.ToLower() ==  'm'.ToString())
+            {
+                Console.WriteLine("Musko");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Zensko");
+
+            }
+            Console.WriteLine($"Tvoje ime je {Ime}, ti imas {godine} godina i tvoj pol je {pol} ");
+        }
+    }   }
---
+++ Projekti_Za_Vezbanje/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekti_Za_Vezbanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int i = random.Next(0, 101);
+            bool pobeda = false;
+
+            do
+            {
+                Console.WriteLine("Pogodi broj");
+                string s = Console.ReadLine();
+                int p = int.Parse(s);
+                if (p > i)
+                {
+                    Console.WriteLine("Broj je prevelik, probaj ponovo");
+                }
+                else if (p < i)
+                {
+                    Console.WriteLine("broj je prenizak, probaj ponovo");
+
+                }
+                else if (p == i) 
+                {
+                    Console.WriteLine("Pogodio si, Svaka Cast");
+                    pobeda = true;
+
+                }
+            } while (pobeda == false);
+
+            
+            
+           
+        }
+    }
+}
